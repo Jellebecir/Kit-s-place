9ac6e1e933ff3d2a406d8586d26fb2b3911d260d
Jellebecir <jellebecir1999@gmail.com>
2025-12-24 14:00:50 +0100

    Added test fetch command to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/blog`, { method: 'GET' })
+      .then((res) => res.json())
+      .then((data) => console.log("API response:", data))
+      .catch((err) => console.error("API error:", err));
+  }, []);
 
   return (
     <>
